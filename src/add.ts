dfb5bf12a0f02e992e0085ce4ad0ed21f52a72d4
Markus Kauppila <markus.kauppila@gmail.com>
2018-09-06 20:19:21 +0300

    Improve the add command

--- src/add.ts
+++ src/add.ts
@@ -2,12 +2,11 @@ import * as fs from 'fs'
 import * as path from 'path'
 import * as crypto from 'crypto'
 import * as zlib from 'zlib'
-import { print } from 'util';
 
 interface TreeObject {
   hash: string
   name: string
-  permissons: string
+  permissons: string // change to number, 0o100644 (0o644)
   type: 'blob' | 'tree'
   deflatedSize: number
 }
@@ -38,8 +37,8 @@ function writeBlob(filePath: string): Promise<TreeObject> {
         const directoryName = hexDigest.slice(0, 2)
         const fileName = hexDigest.slice(2, hexDigest.length)
 
-        console.log(directoryName)
-        console.log(fileName)
+        // console.log(directoryName)
+        // console.log(fileName)
 
         const deflatedBlob = zlib.deflateSync(Buffer.from(blob, 'utf-8'))
         // deflatedBlob.byteLength
@@ -60,47 +59,47 @@ function writeBlob(filePath: string): Promise<TreeObject> {
 }
 
 function writeTree(name: string, to: TreeObject) {
-  // proof of concept
-  // const bb = Buffer.from('887ae9333d92a1d72400c210546e28baa1050e44', 'hex')
-  // const fd = fs.openSync('./hashTest', 'w')
-  // const a = `100644 folder\0${bb.toString('binary')}`
-  // console.log(a)
-  // fs.writeSync(fd, a.slice(0, a.length - 1), undefined, 'binary')
-  // fs.close(fd, (err) => { if (err) console.log('err 1', err)})
+  console.log('----------------------')
+  console.log('to', to)
 
-  const hashBuffer = Buffer.from(to.hash, 'hex')
-  const data = `${to.permissons} ${to.name}\0${hashBuffer.toString('binary')}`
-  const header = `tree ${data.length}\0`
-  const blob = header + data
+  const data = Buffer.concat([
+    Buffer.from('100644'.replace(/^0/, '')), // wtf?
+    Buffer.from(' '),
+    Buffer.from(to.name, 'utf-8'),
+    Buffer.from('\0'),
+    Buffer.from(to.hash, 'hex'),
+    // let oid = Buffer.from(entry.oid.match(/../g).map(n => parseInt(n, 16)))
+    // Buffer.from(to.hash.match(/../g)!.map(n => parseInt(n, 16)))
+  ])
+
+  const blob = Buffer.concat([
+    Buffer.from(`tree ${data.byteLength.toString()}\0`),
+    Buffer.from(data),
+  ])
+
 
   const sha = crypto.createHash('sha1')
   sha.update(blob)
   const hexDigest = sha.digest('hex')
-  console.log(hexDigest)
+  console.log('hex digest', hexDigest)
+
+  const deflatedBlob = zlib.deflateSync(blob)
 
   const directoryName = hexDigest.slice(0, 2)
-  const fileName = hexDigest.slice(2, to.hash.length)
+  const fileName = hexDigest.slice(2, hexDigest.length)
 
-  const deflatedBlob = zlib.deflateSync(Buffer.from(blob, 'binary'))
+  // TODO: second byte is wrong
+  // this: 9c, git: 01
+  // could the deflate cause this difference (nope)
 
-  // TODO compression is wrong (or doesn't work)
-  // TODO hash is wrong too
+  // creating this file works
+  // real git can read it in another repo
+  // but it just doesn't work here for some reason
+  // could be issue with sub-repository structure?
+  // TODO create a clean repo with this!
 
-  try {
-    fs.mkdirSync(`./.git/objects/${directoryName}`)
-  } catch (e) {
-  }
-  // fs.writeFileSync(`./.git/objects/${directoryName}/${fileName}`, deflatedBlob, { encoding: 'ascii' })
-
-  // file written like this looks more right than file written with fs.writeFileSync
-  // maybe the latter defaults to text files or something
-  const fd = fs.openSync(`./.git/objects/${directoryName}/${fileName}`, 'w')
-  fs.writeSync(fd, deflatedBlob, undefined, 'binary')
-  fs.close(fd, (err) => { if (err) console.log('err 1', err)})
-
-  console.log(`tree '${blob}'`)
-  console.log(directoryName)
-  console.log(fileName)
+  fs.mkdirSync(`./.git/objects/${directoryName}`)
+  fs.writeFileSync(`./.git/objects/${directoryName}/${fileName}`, deflatedBlob)
 }
 
 export async function executeAdd(filePath: string): Promise<void> {
