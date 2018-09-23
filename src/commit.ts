fea2476ae2a6204c96528d064fc49656e10e39b0
Markus Kauppila <markus.kauppila@gmail.com>
2018-09-23 23:01:00 +0300

    Fix commit timestamp and timezone

--- src/commit.ts
+++ src/commit.ts
@@ -1,18 +1,29 @@
 import * as process from 'process'
 import { userInfo, hostname } from 'os'
 
+function formatTimeZoneOffset(offsetInMinutes: number): string {
+  const sign = offsetInMinutes < 0 ? '-' : '+'
+  const hours = Math.abs(Math.floor(offsetInMinutes / 60))
+  const minutes = Math.abs(offsetInMinutes % 60)
+  return sign + (hours < 10 ? '0' : '') + hours + (minutes < 10 ? '0' : '') + minutes
+}
+
 export function executeCommit() {
   // read blobs from index
 
-  // create root tree for the blobs
+  // TODO: create root tree for the blobs
 
   const treeHash = 'TODO'
   const parentHash = 'TODO'
   const realgecos = userInfo().username
   const realemail = `${userInfo().username}@${hostname()}`
-  // realdate should be ctime() format
-  // see: https://www.tutorialspoint.com/c_standard_library/c_function_ctime.htm
-  const realdate = Date.now().toLocaleString('Www Mmm dd hh:mm:ss yyyy')
+  // "It [Date format] is `<unix timestamp> <time zone offset>`, where `<unix
+  // timestamp>` is the number of seconds since the UNIX epoch.
+  // `<time zone offset>` is a positive or negative offset from UTC.
+  // For example CET (which is 1 hour ahead of UTC) is `+0100`."
+  // [https://github.com/git/git/blob/master/Documentation/date-formats.txt]
+  const now = new Date()
+  const realdate = `${Math.floor(now.valueOf() / 1000)} ${formatTimeZoneOffset(now.getTimezoneOffset())}`
   // read gecos and email from .git/config
   const gecos = 'John Doe' || realgecos
   const email = 'joh.doe@unko.wn'
